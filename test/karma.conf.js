f3be9726e1f5bf0cd4e3178dbd37bbe563c6f01d
Kira Cheung <killercentury@gmail.com>
2015-07-21 16:50:04 +1000

    add karma-coverage

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -60,7 +60,8 @@ module.exports = function(config) {
     // Which plugins to enable
     plugins: [
       "karma-phantomjs-launcher",
-      "karma-jasmine"
+      "karma-jasmine",
+      'karma-coverage'
     ],
 
     // Continuous Integration mode
